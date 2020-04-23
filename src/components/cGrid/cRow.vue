89caf7ac0722c5037eeb016b8b0cebef03a9299d
chenzh <czh533143@163.com>
2020-04-23 23:23:26 +0800

    设置row

---
+++ src/components/cGrid/cRow.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="row" :style="rowStyle" :class="rowClass">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    gutter: {
+      type: [Number, String]
+    },
+    align: {
+      type: String
+    }
+  },
+  computed: {
+    rowStyle() {
+      return {
+        marginLeft: -this.gutter / 2 + "px",
+        marginRight: -this.gutter / 2 + "px"
+      };
+    },
+    rowClass() {
+      let { align } = this;
+      return [align && `align-${align}`];
+    }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter;
+    });
+  }
+};
+</script>
+<style scoped lang="scss">
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  &.align-left {
+    justify-content: flex-start;
+  }
+  &.align-right {
+    justify-content: flex-end;
+  }
+  &.align-center {
+    justify-content: center;
+  }
+}
+</style>
